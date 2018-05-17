9d97790d8b2f7d4b8c2b8b24e6dc8fece7037de2
Nikita Antonenkov <krigar1184@gmail.com>
2018-05-17 15:25:00 +0300

    Fix

--- test.py
+++ test.py
@@ -3,11 +3,10 @@ import functools
 import tempfile
 
 
-f = tempfile.NamedTemporaryFile(delete=False)
+f = tempfile.NamedTemporaryFile(delete=True)
 
 
 async def read():
-    print('reading from {}'.format(f.name))
     f.seek(0)
     result = f.read().decode('utf8')
     print('read {}'.format(result))
@@ -16,13 +15,15 @@ async def read():
 
 async def write(i):
     print('writing {} to {}'.format(i, f.name))
-    f.write(bytes(i))
     await asyncio.sleep(0)
+    f.write(bytes(str(i).encode('utf8')))
     print('written {}'.format(i))
     return await read()
 
 
 loop = asyncio.get_event_loop()
+q = asyncio.Queue(loop=loop)
+
 future = asyncio.gather(*[asyncio.ensure_future(write(i)) for i in range(10)])
 future.add_done_callback(functools.partial(print, 'pewpew'))
 loop.run_until_complete(future)
